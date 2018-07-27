db019ec1918c0fc9cc18d291d3ec27223aac720c
Kamil Rytarowski <n54@gmx.com>
2018-07-28 00:37:13 +0200

    Checkpoint
    Add tests for C and C++.

---
+++ tests/lib/libc/misc/t_ubsanxx.cpp
@@ -0,0 +1,161 @@
+/*	$NetBSD$	*/
+
+/*-
+ * Copyright (c) 2018 The NetBSD Foundation, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__COPYRIGHT("@(#) Copyright (c) 2018\
+ The NetBSD Foundation, inc. All rights reserved.");
+__RCSID("$NetBSD$");
+
+#include <atf-c.h>
+
+
+ATF_TC(add_overflow_signed);
+ATF_TC_HEAD(add_overflow_signed, tc)
+{
+        atf_tc_set_md_var(tc, "descr",
+	    "Checks signed-integer-overflow, the addition operator");
+}
+
+ATF_TC_BODY(add_overflow_signed, tc)
+{
+	volatile int8_t a = INT8_MAX;
+	volatile int8_t b = atoi("1");
+
+	usleep((a + b) ? 1 : 2);
+}
+
+ATF_TC(add_overflow_unsigned);
+ATF_TC_HEAD(add_overflow_unsigned, tc)
+{
+        atf_tc_set_md_var(tc, "descr",
+	    "Checks unsigned-integer-overflow, the addition operator");
+}
+
+ATF_TC_BODY(add_overflow_unsigned, tc)
+{
+	volatile uint8_t a = UINT8_MAX;
+	volatile uint8_t b = atoi("1");
+
+	usleep((a + b) ? 1 : 2);
+}
+
+ATF_TC(builtin_unreachable);
+ATF_TC_HEAD(builtin_unreachable, tc)
+{
+        atf_tc_set_md_var(tc, "descr", "Checks unreachable");
+}
+
+ATF_TC_BODY(builtin_unreachable, tc)
+{
+	volatile int8_t a = 1;
+	volatile int8_t b = atoi("1");
+
+	if (a == b) {
+		__builtin_unreachable();
+	}
+}
+
+ATF_TC(divrem_overflow_signed_div);
+ATF_TC_HEAD(divrem_overflow_signed_div, tc)
+{
+        atf_tc_set_md_var(tc, "descr",
+	    "Checks signed-integer-overflow, the division operator");
+}
+
+ATF_TC_BODY(divrem_overflow_signed_div, tc)
+{
+	volatile int8_t a = INT8_MIN;
+	volatile int8_t b = atoi("-1");
+
+	usleep((a / b)  ? 1 : 2);
+}
+
+ATF_TC(divrem_overflow_signed_mod);
+ATF_TC_HEAD(divrem_overflow_signed_mod, tc)
+{
+        atf_tc_set_md_var(tc, "descr",
+	    "Checks signed-integer-overflow, the modulo operator");
+}
+
+ATF_TC_BODY(divrem_overflow_signed_mod, tc)
+{
+	volatile int8_t a = INT8_MIN;
+	volatile int8_t b = atoi("-1");
+
+	usleep((a % b) ? 1 : 2);
+}
+
+ATF_TC(function_type_mismatch);
+ATF_TC_HEAD(function_type_mismatch, tc)
+{
+        atf_tc_set_md_var(tc, "descr",
+	    "Checks function type mismatch");
+}
+
+void
+fun1(void)
+{
+}
+
+ATF_TC_BODY(function_type_mismatch, tc)
+{
+
+	((void(*)(int)) ((uintptr_t)(fun1))) (1);
+}
+
+ATF_TP_ADD_TCS(tp)
+{
+
+	ATF_TP_ADD_TC(tp, add_overflow_signed);
+	ATF_TP_ADD_TC(tp, add_overflow_unsigned);
+	ATF_TP_ADD_TC(tp, builtin_unreachable);
+//	ATF_TP_ADD_TC(tp, cfi_bad_type);	// Unimplemented
+//	ATF_TP_ADD_TC(tp, cfi_check_fail);	// Unimplemented
+	ATF_TP_ADD_TC(tp, divrem_overflow_signed_div);
+	ATF_TP_ADD_TC(tp, divrem_overflow_signed_mod);
+//	ATF_TP_ADD_TC(tp, dynamic_type_cache_miss); // Not supported in uBSan
+	ATF_TP_ADD_TC(tp, float_cast_overflow);
+	ATF_TP_ADD_TC(tp, function_type_mismatch);
+	ATF_TP_ADD_TC(tp, invalid_builtin);
+	ATF_TP_ADD_TC(tp, load_invalid_value);
+	ATF_TP_ADD_TC(tp, missing_return);
+	ATF_TP_ADD_TC(tp, mul_overflow);
+	ATF_TP_ADD_TC(tp, negate_overflow);
+	ATF_TP_ADD_TC(tp, nonnull_arg);
+	ATF_TP_ADD_TC(tp, nonnull_return);
+	ATF_TP_ADD_TC(tp, nullability_arg);
+	ATF_TP_ADD_TC(tp, nullability_return);
+	ATF_TP_ADD_TC(tp, out_of_bounds);
+	ATF_TP_ADD_TC(tp, pointer_overflow);
+	ATF_TP_ADD_TC(tp, shift_out_of_bounds);
+	ATF_TP_ADD_TC(tp, sub_overflow);
+	ATF_TP_ADD_TC(tp, type_mismatch);
+	ATF_TP_ADD_TC(tp, vla_bound_not_positive);
+
+	return atf_no_error();
+}
